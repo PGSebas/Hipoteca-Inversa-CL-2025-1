b1a67500233217c1c1b2baa3027de6ab9cde17d4
Your Name <you@example.com>
2025-07-31 11:41:14 -0500

    Funciones iniciales (solo nombre)

---
+++ calculo.py
@@ -0,0 +1,18 @@
+
+def calcular_porcentaje_financiado(edad):
+    return None
+
+def calcular_plazo_financiacion(edad):
+    return None
+
+def calcular_pago_mensual(porcentaje_financiado, valor_vivienda, plazo):
+    return None
+
+def calcular_valor_financiado(porcentaje_financiado, valor_vivienda):
+    return None
+
+def calcular_intereses(tasa_efectiva_anual, pago_mensual):
+    return None
+
+def probar_porcentaje_financiado():
+    return None
