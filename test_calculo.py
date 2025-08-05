b183f4ad54c2da79e510880b40271ed7d47edad8
Juan Sebastian Pinilla Giraldo <pinillagiraldoj@gmail.com>
2025-08-05 11:21:47 -0500

    test plazo financiacion

--- test_calculo.py
+++ test_calculo.py
@@ -12,6 +12,15 @@ class TestFinanciacion(unittest.TestCase):
         # Verificación
         self.assertEqual(resultado, 30)
 
+    def test_plazo_financiacion(self):
+        #Entrada
+        edad = 68
+        
+        # Resultado esperado
+        resultado = calcular_plazo_financiacion(edad)
+
+        # Verificación
+        self.assertEqual(resultado, 20)
 
 if __name__ == '__main__':
     unittest.main()
