5986a5adfc307e2234680131daf1cb77335908e2
Juan Sebastian Pinilla Giraldo <pinillagiraldoj@gmail.com>
2025-08-11 14:31:16 -0500

    Reiniciados los test, ahora se subdivira cada caso en 4 test

--- test_calculo.py
+++ test_calculo.py
@@ -1,38 +1,19 @@
 import unittest
 from calculo import *
 
-class TestFinanciacion(unittest.TestCase):
-    def test_porcentaje_financiado(self):
-        #Entrada
-        edad = 68
-
-        # Calculo 
+class TestHipotecaInversa(unittest.TestCase):
+    # =========================
+    # CASO 1
+    # =========================
+    def test_caso_1_porcentaje(self):
+        # Entrada
+        edad = 68.0
+        # Cálculo
         resultado = calcular_porcentaje_financiado(edad)
-
         # Verificación
         self.assertEqual(resultado, 30)
 
-    def test_plazo_financiacion(self):
-        #Entrada
-        edad = 68
-        
-        # Calculo
-        resultado = calcular_plazo_financiacion(edad)
 
-        # Verificación
-        self.assertEqual(resultado, 20)
 
-    def test_pago_mensual(self):
-        #Entrada
-        porcentaje_financiado = 30
-        valor_vivienda = 500000000
-        plazo = 20
-        
-        # Calculo
-        resultado = calcular_pago_mensual(porcentaje_financiado, valor_vivienda, plazo)
-
-        # Verificación
-        self.assertEqual(resultado, 625000.0)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
