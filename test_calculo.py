9a5fb21ee7b0a3bdfbe881657fec518f9fd7a7c6
Juan Sebastian Pinilla Giraldo <pinillagiraldoj@gmail.com>
2025-08-11 14:33:33 -0500

    test caso 3 plazo

--- test_calculo.py
+++ test_calculo.py
@@ -95,6 +95,14 @@ class TestHipotecaInversa(unittest.TestCase):
         # Verificación
         self.assertEqual(resultado, 55)
 
+    def test_caso_3_plazo(self):
+        # Entrada
+        edad = 80.0
+        # Cálculo
+        resultado = calcular_plazo_financiacion(edad)
+        # Verificación
+        self.assertEqual(resultado, 5)
+
 
 
 if __name__ == "__main__":
