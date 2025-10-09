4da4a1b650fbb852d52c8c57c07369e16d8c5e54
Julian Osorio <julidoso0123@gmail.com>
2025-10-09 00:29:16 -0500

    creacion clase vivienda

---
+++ src/model/vivienda.py
@@ -0,0 +1,11 @@
+class vivienda  :
+    def __init__(self,ciudad_vivienda,
+        barrio_vivienda,
+        direccion_vivienda,
+        propietario_vivienda,
+        valor_vivienda):
+        self.ciudad_vivienda = ciudad_vivienda
+        self.barrio_vivienda = barrio_vivienda
+        self.direccion_vivienda = direccion_vivienda
+        self.propietario_vivienda = propietario_vivienda
+        self.valor_vivienda = valor_vivienda
